3b48a484a4cd69e9d9b9d2ba71e76c53ccaf861f
Alexa Rejas Carrasco <126887187+AlexaRejas@users.noreply.github.com>
2024-10-17 01:45:52 -0500

    Update SpringReactiveApplicationTests.java

--- src/test/java/pe/edu/vallegrande/spring_reactive/SpringReactiveApplicationTests.java
+++ src/test/java/pe/edu/vallegrande/spring_reactive/SpringReactiveApplicationTests.java
@@ -2,12 +2,16 @@ package pe.edu.vallegrande.spring_reactive;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @SpringBootTest
 class SpringReactiveApplicationTests {
 
 	@Test
 	void contextLoads() {
+	    log.info("default test");
 	}
+
 
 }
